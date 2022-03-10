de0004f4ffe8ae0f4872177c9f5fb78354fe3f56
Ahmad Yulian Dinata <Ahmad.Yulian.Dinata@hashmicro.com>
2022-03-11 03:27:23 +0700

    fixing saleorder and workorder

--- models/wizard_cancelled.py
+++ models/wizard_cancelled.py
@@ -7,8 +7,8 @@ from odoo import models, fields, api, exceptions, _
 class Cancelled(models.TransientModel):
     _name = "cancelled.wo"
 
-    note = fields.Text('Note')
+    notes = fields.Text('notes')
 
     def cancelled(self):
         cancel = self.env['work.order'].browse(self.env.context['active_id'])
-        cancel_create = cancel.update({'note': self.note, 'state': 'cancelled'})
+        cancel_create = cancel.update({'notes': self.notes, 'state': 'cancelled'})
--- models/workorder.py
+++ models/workorder.py
@@ -6,17 +6,16 @@ from odoo.osv import osv
 import logging
 _logger = logging.getLogger(__name__)
 
-
 class WorkOrder(models.Model):
     _name = 'work.order'
-    _description = 'Work Order'
-    _rec_name = "wo_number"
+    _description = 'Deskripsi Work Order'
+    _rec_name = "work_order_number"
 
-    wo_number = fields.Char(string='WO Number', required=True, readonly=True, copy=False, default=lambda self: _('New'))
-    bo_reference = fields.Many2one('sale.order', readonly=True)
+    work_order_number = fields.Char(string='WO Number', required=True, readonly=True, copy=False, default=lambda self: _('New'))
+    booking_order_reference = fields.Many2one('sale.order', readonly=True)
     team = fields.Many2one('service.team', required=True)
-    team_leader = fields.Many2one('res.users', string='Team Leader', required=True)
-    team_members = fields.Many2many('res.users', string='Team Members')
+    team_leader = fields.Many2one('res.users', string='Ketua Tim', required=True)
+    team_member = fields.Many2many('res.users', string='Anggota Tim')
     planned_start = fields.Datetime(string="Planned Start", required=True)
     planned_end = fields.Datetime(string='Planned End', required=True)
     date_start = fields.Datetime(string='Date Start', readonly=True)
@@ -24,27 +23,27 @@ class WorkOrder(models.Model):
     state = fields.Selection(
         [('pending', 'Pending'), ('in_progress', 'In Progress'), ('done', 'Done'), ('cancelled', 'Cancelled')],
         string='State', default='pending', track_visibility='onchange')
-    note = fields.Text(string='Note')
+    
+    notes = fields.Text(string='notes')
 
     @api.model
     def create(self, vals):
-        if vals.get('wo_number', _('New')) == _('New'):
+        if vals.get('work_order_number', _('New')) == _('New'):
             if 'company_id' in vals:
-                vals['wo_number'] = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code(
+                vals['work_order_number'] = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code(
                     'work.order') or _('New')
             else:
-                vals['wo_number'] = self.env['ir.sequence'].next_by_code('work.order') or _('New')
+                vals['work_order_number'] = self.env['ir.sequence'].next_by_code('work.order') or _('New')
+        
         result = super(WorkOrder, self).create(vals)
+       
         return result
 
     def start_work(self):
         return self.write({'state': 'in_progress', 'date_start': str(datetime.now())})
-
     def end_work(self):
         return self.write({'state': 'done', 'date_end': str(datetime.now())})
-
     def reset(self):
         return self.write({'state': 'pending', 'date_start': ''})
-
     def cancel(self):
         return self.write({'state': 'cancelled'})
