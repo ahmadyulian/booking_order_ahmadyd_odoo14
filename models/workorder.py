ec23d3a089961db78a8d93358d00aa41d175725a
Ahmad Yulian Dinata <Ahmad.Yulian.Dinata@hashmicro.com>
2022-03-19 00:39:19 +0700

    Fix and Modified Work Order

--- models/workorder.py
+++ models/workorder.py
@@ -1,50 +1,12 @@
-from odoo import fields, models, api, _
-
+from odoo import api, fields, models, _
 
 class WorkOrder(models.Model):
     _name = 'work.order'
-    _description = 'Work Order'
+    _description = 'Deskripsi Work Order'
     _rec_name = "wo_number"
 
-    wo_number = fields.Char(
-        string='WO Number',
-        required=True,
-        readonly=True,
-        copy=False,
-        default=lambda self: _('New'))
-    bo_reference = fields.Many2one(
-        comodel_name='sale.order',
-        readonly=True)
-    team = fields.Many2one(
-        comodel_name='service.team',
-        required=True)
-    team_leader = fields.Many2one(
-        comodel_name='res.users',
-        string='Team Leader',
-        required=True)
-    team_members = fields.Many2many(
-        comodel_name='res.users',
-        string='Team Members')
-    planned_start = fields.Datetime(
-        string="Planned Start",
-        required=True)
-    planned_end = fields.Datetime(
-        string='Planned End',
-        required=True)
-    date_start = fields.Datetime(
-        string='Date Start',
-        readonly=True)
-    date_end = fields.Datetime(
-        string='Date End',
-        readonly=True)
-    state = fields.Selection(
-        [('pending', 'Pending'), ('in_progress', 'In Progress'), ('done', 'Done'), ('cancelled', 'Cancelled')],
-        string='State',
-        default='pending',
-        track_visibility='onchange')
-    note = fields.Text(
-        string='Note')
-
+    #ofchar #oofcompute
+    wo_number = fields.Char(string='WO Number', copy=False, default=lambda self: _('New'), readonly=True, required=True)
     @api.model
     def create(self, vals):
         if vals.get('wo_number', _('New')) == _('New'):
@@ -55,14 +17,36 @@ class WorkOrder(models.Model):
                 vals['wo_number'] = self.env['ir.sequence'].next_by_code('work.order') or _('New')
         return super(WorkOrder, self).create(vals)
 
+    #ofm2o
+    bo_reference = fields.Many2one(comodel_name='sale.order', readonly=True)
+    team = fields.Many2one(comodel_name='service.team', required=True)
+    team_leader = fields.Many2one(comodel_name='res.users', string='Team Leader', required=True)
+
+    #ofm2m
+    team_members = fields.Many2many(comodel_name='res.users',
+        string='Team Members')
+
+    #ofdatetime
+    planned_start = fields.Datetime(string="Planned Start", required=True)
+    planned_end = fields.Datetime( string='Planned End', required=True)
+    date_start = fields.Datetime(string='Date Start', readonly=True)
+    date_end = fields.Datetime(string='Date End', readonly=True)
+
+    #ofsel
+    state = fields.Selection(
+        [('pending', 'Pending'), ('in_progress', 'In Progress'), ('done', 'Done'), ('cancelled', 'Cancelled')],
+        string='State',
+        default='pending',
+        track_visibility='onchange')
+
+    note = fields.Text(
+        string='Note')
+
     def start_work(self):
         return self.write({'state': 'in_progress', 'date_start': fields.Datetime.now()})
-
     def end_work(self):
         return self.write({'state': 'done', 'date_end': fields.Datetime.now()})
-
     def reset(self):
         return self.write({'state': 'pending', 'date_start': ''})
-
     def cancel(self):
         return self.write({'state': 'cancelled'})
